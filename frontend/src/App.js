67d2f46e4a25c48ee2017e6ad39a270e02d6ee00
Shekinah Sardonyx Yadala <195621255+shekinah1998@users.noreply.github.com.>
2026-01-28 19:22:16 -0700

    Fix: Switch to HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import ResumeSection from './components/ResumeSection';
